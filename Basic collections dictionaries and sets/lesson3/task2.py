ba0d40b6bcc2ea70f80a4621d5d654c04f597a0b
Pasik <parasatto@gmail.com>
2023-01-02 20:52:40 +0600

    got it

---
+++ Basic collections dictionaries and sets/lesson3/task2.py
@@ -0,0 +1,25 @@
+players_dict = {
+    1: {'name': 'Vanya', 'team': 'A', 'status': 'Rest'},
+    2: {'name': 'Lena', 'team': 'B', 'status': 'Training'},
+    3: {'name': 'Maxim', 'team': 'C', 'status': 'Travel'},
+    4: {'name': 'Egor', 'team': 'C', 'status': 'Rest'},
+    5: {'name': 'Andrei', 'team': 'A', 'status': 'Training'},
+    6: {'name': 'Sasha', 'team': 'A', 'status': 'Rest'},
+    7: {'name': 'Alina', 'team': 'B', 'status': 'Rest'},
+    8: {'name': 'Masha', 'team': 'C', 'status': 'Travel'}
+}
+
+# Чтобы не прописывать решение "в лоб", вручную подставляя статус и команду - попробуем сформировать дополнительные словарь и список,
+# чтобы автоматизировать этот процесс:
+help_dict = {"Rest": "отдыхают",
+             "Training": "тренируются",
+             "Travel": "путешествуют"}
+
+team_order = ["A", "B", "C"]
+
+# Запустим цикл по словарю состояний и одновременно будем вести счёт состояний, чтобы на каждой итерации выбирать одну из команд:
+for index, state in enumerate(help_dict):
+    print(f"Все члены команды из команды {team_order[index]}, которые {help_dict[state]}:")
+    for _, player in players_dict.items():
+        if player["status"] == state and player["team"] == team_order[index]:
+            print(player["name"])
