909043e6935a2936e9c41a99767f457579116766
Pasik <parasatto@gmail.com>
2023-01-06 15:37:57 +0600

    task7

---
+++ Basic Collections Tuples/practical work/task7.py
@@ -0,0 +1,18 @@
+def tpl_sort(string):
+    status = True
+    for i in string:
+        if type(i) is int:
+            continue
+        else:
+            status = False
+            break
+
+    if status == True:
+        sort = sorted(string)
+        sort = tuple(sort)
+        print(sort)
+    else:
+        print(string)
+
+tuple1 = (6, 3, -1, 8, 4, 10, -5)
+tpl_sort(tuple1)
