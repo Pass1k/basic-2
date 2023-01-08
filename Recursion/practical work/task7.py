6c84136e97dc9141b87ee35a8869150555524b1f
Pasik <parasatto@gmail.com>
2023-01-08 23:02:35 +0600

    task7

---
+++ Recursion/practical work/task7.py
@@ -0,0 +1,13 @@
+def summ(*args):
+    def flaten(a_list):
+        result = []
+        for e in a_list:
+            if isinstance(e, int):
+                result.append(e)
+            else:
+                result.extend(flaten(e))
+        return result
+    return sum(flaten(args))
+
+print(summ([[1, 2, [3]], [1], 3]))
+print(summ(1,2,3,4,5))
